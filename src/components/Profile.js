1d836ad424af9903f246efdb91b10af4fd4ebd4f
Kevin Schoen <kevschoen83@gmail.com>
2022-06-10 14:34:53 -0500

    added react-app

---
+++ src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Profile() {
+    return (
+        <div className="profile">
+            <img src="../images/my-picture.jpg" alt="Kevin Schoen" className="my-image" width={225} />
+            <h1>Kevin Schoen</h1>
+            <p>I have a Master's degree in Game Design <br>
+            </br>from Full Sail University.I have a passion<br>
+            </br> for creating and designing experiences for<br>
+            </br> people, whether that is within a game or not.</p>
+        </div>
+
+    )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Projects() {
+    return (
+        <section className="section">
+            <div className="projects">
+                <h1><u>My Projects</u></h1>
+                <h2><a href="https://github.com/kevschoen83/GenSpark/tree/main/hangman-functional">Hangman</a></h2>
+                <h2><a href="https://github.com/kevschoen83/GenSpark/tree/main/dragon-cave">Dragon Cave</a></h2>
+                <h2><a href="https://github.com/kevschoen83/GenSpark/tree/main/boarding-pass">Boarding Pass</a></h2>
+                <h2><a href="https://github.com/kevschoen83/GenSpark/tree/main/humans-vs-goblins">Humans vs Goblins</a></h2>
+                <h2><a href="https://github.com/kevschoen83/GenSpark/tree/main/Simple%20Form">A Simple Form</a></h2>
+                <h2><a href="https://github.com/kevschoen83/GenSpark/tree/main/RESTFulAssignment/RESTFulAssignment">A RESTful API</a></h2>
+            </div>
+        </section>
+    )
+}
